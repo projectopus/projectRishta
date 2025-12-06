cd5676a6131d5a8af24f9d6340a29e9dce2e4158
projectopus <m@rishta.dedyn.io>
2025-12-07 02:35:11 +0600

    update

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG = {
+  SUPABASE_URL: 'https://zlsfmoojgvrjadlyrfaz.supabase.co',
+  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpsc2Ztb29qZ3ZyamFkbHlyZmF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUwNDQ2NjQsImV4cCI6MjA4MDYyMDY2NH0.U-w6CmybVc3DlZv2OVjrsqHZz65xrvkVlJhkCKSDVEY'
+};
